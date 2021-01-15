91ce3f2a2696361583ef7f80d51f2ff97ce3956c
kghugo <howtostudycantonese@gmail.com>
2021-01-15 10:45:43 +0800

    Update benchmark

--- benchmarks/index.spec.js
+++ benchmarks/index.spec.js
@@ -19,12 +19,12 @@ const next2 = require('./cases/functionWithStringConcat.js')(charList, 0)
 suite.add('Function generator; concat string for result', function() {
 	const id = next2()
 })
-//
-// const next3 = require('./cases/functionWithStringConcatAndMemo.js')(charList, 0)
-//
-// suite.add('Function generator; concat string for result; memoized', function() {
-// 	const id = next3()
-// })
+
+const next3 = require('./cases/functionWithStringConcatAndMemo.js')(charList, 0)
+
+suite.add('Function generator; concat string for result; memoized', function() {
+	const id = next3()
+})
 
 const next4 = require('./cases/functionWithStringConcatAndCustomMemo')(
 	charList,
@@ -32,10 +32,9 @@ const next4 = require('./cases/functionWithStringConcatAndCustomMemo')(
 )
 
 suite.add(
-	'Function generator; concat string for result; custom memo',
+	'Function generator; concat string for result; custom memo(Version 1.0.0)',
 	function() {
 		const id = next4()
-		// console.log('check result', id)
 	}
 )
 
@@ -43,39 +42,39 @@ const generator = require('./cases/generator.js')({
 	chars: charList
 })
 
-// suite.add('No memoized, recursion(Version 0.1.0)', function() {
-// 	const id = generator.next().value
-// })
-//
-// const memoizedGenerator = memoized({
-// 	chars: charList
-// })
-//
-// suite.add('Memoized, recursion', function() {
-// 	const id = memoizedGenerator.next().value
-// })
-//
-// const partialMemoGenerator = partialMemoed({
-// 	chars: charList
-// })
-//
-// suite.add('Partial Memoized, recursion', function() {
-// 	const id = partialMemoGenerator.next().value
-// })
-//
-// const stackoverflowGenerator = new StackoverflowAnswer(charList)
-//
-// suite.add('Array without recursion, Stackover Flow Answer', function() {
-// 	const id = stackoverflowGenerator.next()
-// })
-//
-// const incstrGenerator = incstr.idGenerator({
-// 	alphabet: charList
-// })
-//
-// suite.add('incStr, for-loop', function() {
-// 	const id = incstrGenerator()
-// })
+suite.add('No memoized, recursion(Version 0.1.0)', function() {
+	const id = generator.next().value
+})
+
+const memoizedGenerator = memoized({
+	chars: charList
+})
+
+suite.add('Memoized, recursion', function() {
+	const id = memoizedGenerator.next().value
+})
+
+const partialMemoGenerator = partialMemoed({
+	chars: charList
+})
+
+suite.add('Partial Memoized, recursion', function() {
+	const id = partialMemoGenerator.next().value
+})
+
+const stackoverflowGenerator = new StackoverflowAnswer(charList)
+
+suite.add('Array without recursion, Stackover Flow Answer', function() {
+	const id = stackoverflowGenerator.next()
+})
+
+const incstrGenerator = incstr.idGenerator({
+	alphabet: charList
+})
+
+suite.add('incStr, for-loop', function() {
+	const id = incstrGenerator()
+})
 
 suite.on('cycle', function(event) {
 	if (event.target.error) {
