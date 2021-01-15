464947651df2bcc4da013eeb80a312f81c081ca4
Jacky yu <ych605@gmail.com>
2021-01-15 16:41:23 +0800

    update function with JKY

--- benchmarks/cases/functionWithJKY.js
+++ benchmarks/cases/functionWithJKY.js
@@ -3,20 +3,16 @@ function generateChar(chars, currentPass, cache) {
     let passingCount = currentPass
     
     while (passingCount >= chars.length) {
-        if (cache[passingCount]) {
-            return cache[passingCount] + str
-        }
 		const remainder = passingCount % chars.length
 		passingCount = (passingCount - remainder) / chars.length - 1
         str = chars[remainder] + str
     }
     
-	return cache[currentPass] = chars[passingCount] + str
+	return chars[passingCount] + str
 }
 
 function generator(symbol, index = 0) {
-	const cache = {}
-	return () => generateChar(symbol, index++, cache)
+	return () => generateChar(symbol, index++)
 }
 
 module.exports = generator
